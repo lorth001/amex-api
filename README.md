c16ca105efa9117512de44cd00a0891b46895e47
Luke Orth <lorth001@gmail.com>
2022-04-07 14:05:28 -0500

    Finished script and added details to the README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# amex-api
-Python library for accessing the American Express (AMEX) API
+# Description
+This is a small script I wrote for retrieving user transaction details from American Express (AMEX).
+
+# Background
+AMEX hasn't made their APIs public yet (shame) and it doesn't appear that anyone else has done the dirty work of reverse-engineering their private APIs.  It took a bit of work to figure out, so I wanted to make it available here in case anyone else needs access to the same data.  Of course this script can also be expanded on if you need access to other data (rewards, statements, etc.), but I only needed transactions at the time of creation.
+
+# Configuration
+You will of course need the username and password for the account you are trying to access.  
+
+You'll also need an "account_token", which is a private value that AMEX uses internally.  This "account_token" seems to be unique and directly tied to each account--it never changes.  You can find it in the page source with these steps:
+
+1. Login to AMEX website
+2. Right-click the page and select "View Page Source" (or similar depending on your browser)
+3. Search for "account_token"
+
--- main.py
+++ main.py
@@ -1,1 +1,41 @@
 import requests
+import json
+import keyring      # or whatever credential manager you fancy
+
+USERNAME = 'USERNAME'
+PASSWORD = keyring.get_password('amex_password', USERNAME)
+TOKEN = keyring.get_password('amex_token', USERNAME)    # view README for this
+
+cookie = {
+    'url': 'https://global.americanexpress.com/myca/logon/us/action/login',
+    'payload': f'Logon=Logon&UserID={USERNAME}&Password={PASSWORD}',
+    'headers': {
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+    }
+}
+
+transactions = {
+    'url': 'https://global.americanexpress.com/api/servicing/v1/financials/transactions',
+    'querystring': {
+        'limit': '1000',
+        'offset': '0',
+        'start_date': '2021-10-06',
+        'end_date': '2022-04-06',
+        'extended_details': 'merchant, category',
+        'status': 'posted'
+    },
+    'headers': {
+        'account_token': TOKEN
+    }
+}
+
+
+with requests.Session() as s:
+    # Initial request to set cookies
+    s.request('POST', cookie['url'], data=cookie['payload'], headers=cookie['headers'])
+
+    # Main request to retrieve data
+    response = s.request('GET', transactions['url'], headers=transactions['headers'], params=transactions['querystring']).json()
+
+    print(json.dumps(response, indent=4))
