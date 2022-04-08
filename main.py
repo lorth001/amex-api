da6296d9c40acd2c10a79f90f85ec821eb80da7f
Luke Orth <lorth001@gmail.com>
2022-04-08 15:58:01 -0500

    made some updates to include config file

--- main.py
+++ main.py
@@ -1,41 +1,50 @@
 import requests
 import json
-import keyring      # or whatever credential manager you fancy
+import yaml
+import keyring  # or whatever credential manager you fancy
+from datetime import datetime, timedelta
 
-USERNAME = 'USERNAME'
+# Credentials
+USERNAME = 'USERNAME'   # your username here
 PASSWORD = keyring.get_password('amex_password', USERNAME)
-TOKEN = keyring.get_password('amex_token', USERNAME)    # view README for this
+TOKEN = keyring.get_password('amex_token', USERNAME)    # see README for this
 
-cookie = {
-    'url': 'https://global.americanexpress.com/myca/logon/us/action/login',
-    'payload': f'Logon=Logon&UserID={USERNAME}&Password={PASSWORD}',
-    'headers': {
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-    }
-}
+# Get relevant dates
+today = datetime.now().strftime('%Y-%m-%d')
+week_ago = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
 
-transactions = {
-    'url': 'https://global.americanexpress.com/api/servicing/v1/financials/transactions',
-    'querystring': {
-        'limit': '1000',
-        'offset': '0',
-        'start_date': '2021-10-06',
-        'end_date': '2022-04-06',
-        'extended_details': 'merchant, category',
-        'status': 'posted'
-    },
-    'headers': {
-        'account_token': TOKEN
-    }
-}
+# Load configs
+with open('config.yml', 'r') as file:
+    config = yaml.safe_load(file)
 
+session, transactions = (config['session'], config['transactions'])
+
+# Replace sensitive data in configs
+session['payload'] = session['payload'].replace('{USERNAME}', USERNAME)
+session['payload'] = session['payload'].replace('{PASSWORD}', PASSWORD)
+transactions['headers']['account_token'] = transactions['headers']['account_token'].replace('{TOKEN}', TOKEN)
+
+# Replace dates
+transactions['querystring']['start_date'] = today
+transactions['querystring']['end_date'] = week_ago
 
 with requests.Session() as s:
-    # Initial request to set cookies
-    s.request('POST', cookie['url'], data=cookie['payload'], headers=cookie['headers'])
 
-    # Main request to retrieve data
-    response = s.request('GET', transactions['url'], headers=transactions['headers'], params=transactions['querystring']).json()
+    # Initial request to establish sesssion and set cookies
+    s.request(
+        'POST',
+        session['url'],
+        data=session['payload'],
+        headers=session['headers']
+    )
 
-    print(json.dumps(response, indent=4))
+    # Main request to get transactions
+    response = s.request(
+        'GET',
+        transactions['url'],
+        headers=transactions['headers'],
+        params=transactions['querystring']
+    ).json()
+
+print(json.dumps(response, indent=4))
+
