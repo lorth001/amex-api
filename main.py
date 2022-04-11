07e4da1cf1c77314a548c9fff0397ee5a52c0d26
Luke Orth <lorth001@gmail.com>
2022-04-11 05:07:16 -0500

    Updated code to add pretty json formatting

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 import requests
 import json
 import yaml
-import keyring  # or whatever credential manager you fancy
+import keyring  # or whatever method of credential management you fancy
 from datetime import datetime, timedelta
 
 # Credentials
 USERNAME = 'USERNAME'   # your username here
 PASSWORD = keyring.get_password('amex_password', USERNAME)
-TOKEN = keyring.get_password('amex_token', USERNAME)    # see README for this
+TOKEN = keyring.get_password('amex_token', USERNAME)    # view README for this
 
 # Get relevant dates
 today = datetime.now().strftime('%Y-%m-%d')
 week_ago = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
 
-# Load configs
+# Load configs from file
 with open('config.yml', 'r') as file:
     config = yaml.safe_load(file)
 
 session, transactions = (config['session'], config['transactions'])
 
-# Replace sensitive data in configs
+# Replace sensitive data from configs
 session['payload'] = session['payload'].replace('{USERNAME}', USERNAME)
 session['payload'] = session['payload'].replace('{PASSWORD}', PASSWORD)
 transactions['headers']['account_token'] = transactions['headers']['account_token'].replace('{TOKEN}', TOKEN)
 
-# Replace dates
-transactions['querystring']['start_date'] = today
-transactions['querystring']['end_date'] = week_ago
+# Replace dates from configs
+transactions['querystring']['start_date'] = week_ago
+transactions['querystring']['end_date'] = today
 
 with requests.Session() as s:
-
+    
     # Initial request to establish sesssion and set cookies
     s.request(
         'POST',
@@ -44,7 +44,7 @@ with requests.Session() as s:
         transactions['url'],
         headers=transactions['headers'],
         params=transactions['querystring']
-    ).json()
+    ).json()['transactions']
 
-print(json.dumps(response, indent=4))
+    print(json.dumps(response, indent=4))
 
